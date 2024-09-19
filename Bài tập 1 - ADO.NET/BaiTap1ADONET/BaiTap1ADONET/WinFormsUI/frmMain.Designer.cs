399b649051cbbc6ed2a76630719144af170bf218
duc190604 <haminhduchaminhduc@gmail.com>
2024-09-19 20:25:55 +0700

    hoàn thành bài 1

---
+++ Bài tập 1 - ADO.NET/BaiTap1ADONET/BaiTap1ADONET/WinFormsUI/frmMain.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace BaiTap1ADONET
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoLuongDe = new System.Windows.Forms.TextBox();
+            this.txtSoLuongCuu = new System.Windows.Forms.TextBox();
+            this.txtSoLuongBo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtSoLuongDe);
+            this.panel1.Controls.Add(this.txtSoLuongCuu);
+            this.panel1.Controls.Add(this.txtSoLuongBo);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(206, 59);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(958, 180);
+            this.panel1.TabIndex = 0;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label4.Location = new System.Drawing.Point(697, 121);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.label4.Size = new System.Drawing.Size(95, 30);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Số lượng dê";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label3.Location = new System.Drawing.Point(344, 121);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.label3.Size = new System.Drawing.Size(100, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Số lượng cừu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.label2.Location = new System.Drawing.Point(1, 121);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.label2.Size = new System.Drawing.Size(95, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Số lượng bò";
+            // 
+            // txtSoLuongDe
+            // 
+            this.txtSoLuongDe.Location = new System.Drawing.Point(821, 127);
+            this.txtSoLuongDe.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSoLuongDe.Name = "txtSoLuongDe";
+            this.txtSoLuongDe.Size = new System.Drawing.Size(132, 22);
+            this.txtSoLuongDe.TabIndex = 3;
+            this.txtSoLuongDe.TextChanged += new System.EventHandler(this.txtSoLuongDe_TextChanged);
+            // 
+            // txtSoLuongCuu
+            // 
+            this.txtSoLuongCuu.Location = new System.Drawing.Point(469, 127);
+            this.txtSoLuongCuu.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSoLuongCuu.Name = "txtSoLuongCuu";
+            this.txtSoLuongCuu.Size = new System.Drawing.Size(132, 22);
+            this.txtSoLuongCuu.TabIndex = 2;
+            this.txtSoLuongCuu.TextChanged += new System.EventHandler(this.txtSoLuongCuu_TextChanged);
+            // 
+            // txtSoLuongBo
+            // 
+            this.txtSoLuongBo.Location = new System.Drawing.Point(116, 127);
+            this.txtSoLuongBo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSoLuongBo.Name = "txtSoLuongBo";
+            this.txtSoLuongBo.Size = new System.Drawing.Size(132, 22);
+            this.txtSoLuongBo.TabIndex = 1;
+            this.txtSoLuongBo.TextChanged += new System.EventHandler(this.txtSoLuongBo_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.RosyBrown;
+            this.label1.Location = new System.Drawing.Point(4, 33);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(949, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập số lượng";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.panel2.Controls.Add(this.dataGrid);
+            this.panel2.Controls.Add(this.btnThongKe);
+            this.panel2.Location = new System.Drawing.Point(299, 287);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(774, 376);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column5,
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGrid.GridColor = System.Drawing.SystemColors.AppWorkspace;
+            this.dataGrid.Location = new System.Drawing.Point(25, 99);
+            this.dataGrid.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.RowHeadersWidth = 51;
+            this.dataGrid.RowTemplate.Height = 24;
+            this.dataGrid.Size = new System.Drawing.Size(720, 242);
+            this.dataGrid.TabIndex = 8;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "AnimalType";
+            this.Column5.HeaderText = "Loại";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 125;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "CurrentAmount";
+            this.Column1.HeaderText = "Số lượng hiện tại";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            this.Column1.Width = 150;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "TotalGiveBirth";
+            this.Column2.HeaderText = "Số lượng sinh";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "TotalMilk";
+            this.Column3.HeaderText = "Sữa";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "TotalAmount";
+            this.Column4.HeaderText = "Tổng số lượng";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 125;
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.BackColor = System.Drawing.Color.Red;
+            this.btnThongKe.Location = new System.Drawing.Point(265, 17);
+            this.btnThongKe.Margin = new System.Windows.Forms.Padding(4);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(188, 64);
+            this.btnThongKe.TabIndex = 0;
+            this.btnThongKe.Text = "Thống kê";
+            this.btnThongKe.UseVisualStyleBackColor = false;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1379, 772);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMain";
+            this.Text = "Quản lí trang trại";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoLuongDe;
+        private System.Windows.Forms.TextBox txtSoLuongCuu;
+        private System.Windows.Forms.TextBox txtSoLuongBo;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
+
