835f09ee34e2ea7e2c37bf8e3656ac8ed1063e80
duc190604 <haminhduchaminhduc@gmail.com>
2024-10-09 15:36:58 +0700

    Bai 4

---
+++ Bai4_MVC/WebsiteQuanLySanPham/WebsiteQuanLySanPham/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteQuanLySanPham.Models;
+namespace WebsiteQuanLySanPham.Controllers
+{
+    public class CatalogController : Controller
+    {
+        // GET: Catalog
+        public ActionResult Index()
+        {
+            string connectionString = "LAPTOP-M863II0B\\SQLEXPRESS";
+            QuanLySanPhamDataContext context= new QuanLySanPhamDataContext(connectionString);
+            List<Catalog> dsCatalog= context.Catalogs.ToList();
+            return View(dsCatalog);
+        }
+    }
+}
---
+++ Bai4_MVC/WebsiteQuanLySanPham/WebsiteQuanLySanPham/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteQuanLySanPham.Models;
+
+namespace WebsiteQuanLySanPham.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            string connectionString = "LAPTOP-M863II0B\\SQLEXPRESS";
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(connectionString);
+            List<Product> dsProduct = context.Products.ToList();
+            return View(dsProduct);
+        }
+    }
+}
